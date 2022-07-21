0bfb98539dbac84e5fab210b8019e7070ad41624
Sourav Paul <sourav.paul@innoraft.com>
2022-07-21 12:54:54 +0530

    all courses

---
+++ internship codes/python codes/code1.py
@@ -0,0 +1,7 @@
+def welcome(name,DOJ):
+    print(f"Welcome {name} in innoraft",end=" ")
+    print("your Date of joining is {DOJ}")
+
+name= input("Enter your name: " )
+DOJ= input("enter date of joining: ")
+welcome(name,DOJ)
